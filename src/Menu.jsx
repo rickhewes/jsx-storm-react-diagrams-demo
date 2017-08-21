60a1187c990d06f63c7a9a6fa5c9c4169f261207
Rick <rick.hewes@mayden.co.uk>
2017-08-21 11:35:59 +0100

    Tidy menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import List, { ListItem, ListItemText } from 'material-ui/List';
 
 class Menu extends React.Component {
 	render() {
 		return (
 			<div>
-				<ul>
-					<li>
-						<Link to={`/demo1`}>Demo 1</Link>
-					</li>
-					<li>
-						<Link to={`/demo2`}>Demo 2</Link>
-					</li>
-				</ul>
+				<List>
+					<ListItem button component="a" href="/demo1">
+						<ListItemText primary="Demo 1" />
+					</ListItem>
+					<ListItem button component="a" href="/demo2">
+						<ListItemText primary="Demo 2" />
+					</ListItem>
+				</List>
 			</div>
 		);
 	}
